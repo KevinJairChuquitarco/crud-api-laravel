a7a14bc4fdaf89000fff46eabdb773d37f6e953a
KevinJairChuquitarco <kevinchuquitarco11@gmail.com>
2024-08-12 09:18:08 -0500

    Api Finalizada

--- app/Http/Controllers/EstudianteController.php
+++ app/Http/Controllers/EstudianteController.php
@@ -62,6 +62,83 @@ class EstudianteController extends Controller
         ];
 
         return response()->json($data, 201);
+    }
+    //Función para obtener un sólo estudiante
+    public function show($id){
+        $estudiante = Estudiante::find($id);
+        if (!$estudiante) {
+            $data =[
+                'mensaje'=>'Estudiante no encontrado',
+                'estado'=> 404
+            ]; 
+            return response()->json($data, 404);
+        }
 
+        $data = [
+            'estudiante'=>$estudiante,
+            'estado'=>200
+        ];
+        return response()->json($data, 200);
+    }
+    //Función para eliminar un estudiante
+    public function destroy($id){
+        $estudiante = Estudiante::find($id);
+        if (!$estudiante) {
+            $data =[
+                'mensaje'=>'Estudiante no encontrado',
+                'estado'=> 404
+            ]; 
+            return response()->json($data, 404);
+        }
+        $estudiante->delete();
+        $data = [
+            'mensaje'=>'Estudiante eliminado',
+            'estado' => 200
+        ];
+        return response()->json($data, 200);
+    }
+
+    //Función para actualizar un estudiante
+    public function update(Request $request, $id){
+
+        $estudiante = Estudiante::find($id);
+        
+        if (!$estudiante) {
+            $data =[
+                'mensaje'=>'Estudiante no encontrado',
+                'estado'=> 404
+            ]; 
+            return response()->json($data, 404);
+        }
+
+        $validator = Validator::make($request->all(),[
+            'nombre'=>'required|max:255',
+            'email'=>'required|email|unique:estudiante',
+            'telefono'=>'required|digits:10',
+            'lenguaje'=>'required|in:Español,Ingles,Frances',
+        ]);
+
+        if ($validator->fails()) {
+            $data = [
+                'mensaje'=>'Error en la validación de datos',
+                'error'=>$validator->errors(),
+                'estado'=>400
+            ];
+            return response()->json($data, 400);
+        }
+        
+        $estudiante->nombre = $request->nombre; 
+        $estudiante->email = $request->email; 
+        $estudiante->telefono = $request->telefono; 
+        $estudiante->lenguaje = $request->lenguaje; 
+
+        $estudiante->save();
+
+        $data = [
+            'estudiante'=>$estudiante,
+            'estado'=>200
+        ];
+
+        return response()->json($data, 200);
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -8,3 +8,6 @@ use App\Http\Controllers\EstudianteController;
 
 Route::get('estudiantes', [EstudianteController::class,'index']);
 Route::post('estudiantes', [EstudianteController::class,'store']);
+Route::get('estudiante/{id}', [EstudianteController::class,'show']);
+Route::delete('estudiante/{id}', [EstudianteController::class,'destroy']);
+Route::put('estudiante/{id}', [EstudianteController::class,'update']);
